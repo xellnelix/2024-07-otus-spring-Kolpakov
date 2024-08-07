d7b703a5079f5595c4a2228cbee31f4ec6f095ca
xellnelix <xellnelix@gmail.com>
2024-08-07 22:23:55 +0300

    Homework has done

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,10 +1,16 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,7 +23,22 @@ public class CsvQuestionDao implements QuestionDao {
         // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
+            if (is == null) {
+                throw new QuestionReadException("File is empty!");
+            }
 
-        return new ArrayList<>();
+            InputStreamReader inputStreamReader = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            List<QuestionDto> questionDtoList = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse();
+            return questionDtoList.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException e) {
+            throw new QuestionReadException(e.getMessage());
+        }
     }
 }
