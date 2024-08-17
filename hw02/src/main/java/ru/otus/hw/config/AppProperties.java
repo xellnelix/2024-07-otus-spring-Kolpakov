a20f2e947397d85b9b5c2313f6103b9431535f22
xellnelix <xellnelix@gmail.com>
2024-08-17 18:12:13 +0300

    Added annotation-based configuration

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,13 +2,19 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application.properties")
+@Configuration
 @Data
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
+    @Value("${test.rightAnswersCountToPass}")
     private int rightAnswersCountToPass;
 
     // внедрить свойство из application.properties
+    @Value("${test.fileName}")
     private String testFileName;
 }
