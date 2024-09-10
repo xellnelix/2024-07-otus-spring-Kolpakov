4a6fa163087672a2a7c892c7df35f2945503d124
Maxim Kolpakov <KolpakovMS@hq.corp.mos.ru>
2024-09-10 12:23:33 +0300

    Copied hw02

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,25 +1,39 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
-@Component
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName());
+             InputStreamReader inputStreamReader = new InputStreamReader(is);
+             BufferedReader reader = new BufferedReader(inputStreamReader)) {
 
-        return new ArrayList<>();
+            List<QuestionDto> questionDtoList = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse();
+            return questionDtoList.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException | NullPointerException e) {
+            throw new QuestionReadException("File reading error: ", e);
+        }
     }
 }
--- hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,31 +3,51 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
-    private final LocalizedIOService ioService;
+    private final IOService ioService;
 
     private final QuestionDao questionDao;
 
     @Override
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
-        ioService.printLineLocalized("TestService.answer.the.questions");
-        ioService.printLine("");
-
+        ioService.printFormattedLine("Please answer the questions below%n");
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (var question : questions) {
+            printQuestion(question);
+            var studentAnswer = ioService.readString();
+            var isAnswerValid = checkCorrectAnswer(question.answers(), studentAnswer);
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
     }
 
+    private void printQuestion(Question question) {
+        ioService.printLine(question.text());
+        for (var answer : question.answers()) {
+            ioService.printFormattedLine(answer.text());
+        }
+    }
+
+    private boolean checkCorrectAnswer(List<Answer> answerList, String studentAnswer) {
+        for (var answer : answerList) {
+            if (Objects.equals(studentAnswer, answer.text()) && answer.isCorrect()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
